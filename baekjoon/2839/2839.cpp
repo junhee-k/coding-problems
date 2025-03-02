5a656f3c3670dda3652e5c53688050989caae334
junhee-k <rlawnsgml0919@gmail.com>
2025-03-03 00:45:31 +0900

    Solved 2 DP problems

---
+++ baekjoon/2839/2839.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int dp[5000];
+
+int main() {
+    int N;
+    cin >> N;
+    dp[3] = 1;
+    dp[5] = 1;
+
+    for(int i = 6; i <= N; i++){
+        if(dp[i - 3]) dp[i] = dp[i-3] + 1;
+        if(dp[i - 5]) dp[i] = dp[i] ? min(dp[i], dp[i-5] + 1) : dp[i-5]+1;
+    }
+    cout << (dp[N] == 0 ? -1 : dp[N]) << '\n';
+    return 0;
+}
