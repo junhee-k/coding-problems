505ffbf358a9e07fb7f4a388600ac80bbd295098
junhee-k <rlawnsgml0919@gmail.com>
2025-03-08 18:23:49 +0900

    Solved boj 1520

--- baekjoon/dp/1520.cpp
+++ baekjoon/dp/1520.cpp
@@ -38,3 +38,16 @@ int main() {
 
     cout << DFS(0,0);
 }
+
+/*
+This solution combines DFS with dynamic programming to
+find the # of paths to the n,m index of the matrix. The
+dp[i][j] value is set to represent the number of paths 
+to position (n-1,m-1) from the position (i,j). If the value
+is -1, it means that a path to position (n-1,m-1) has not
+been computed yet. At the end of the program, any 
+dp[i][j] == -1 means that the DFS algorithm did reach that
+position. As the DFS traces back, it adds the number of paths
+to (n-1,m-1) until 0,0. The answer of the question is stored
+in dp[0][0], which is what is returned at the end.
+*/
